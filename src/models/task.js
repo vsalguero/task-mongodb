ead59e263b5d0ed45ab8284f0bd163d4298b3620
Vladimir Salguero <vladimirsalguero891@gmail.com>
2021-10-25 23:04:00 -0600

    Model task

--- src/models/task.js
+++ src/models/task.js
@@ -2,4 +2,13 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-new Schema 
+const TaskSchema = new Schema({
+    title: String,
+    description: String,
+    status: {
+        type: Boolean,
+        default : false
+    }
+})
+
+module.export = mongoose.model('task', TaskSchema);
